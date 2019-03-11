24e99c8f44e7ce0500764bc13be93a24ad4b5ec0
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-03-11 11:15:59 -0700

    Fix FU540-L2 vtable
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- metal_header/sifive_fu540_c000_l2.h
+++ metal_header/sifive_fu540_c000_l2.h
@@ -41,7 +41,7 @@ class sifive_fu540_c000_l2 : public Device {
 	  emit_struct_begin("sifive_fu540_c000_l2", n);
 
 	  emit_struct_field("vtable", "&__metal_driver_vtable_sifive_fu540_c000_l2");
-	  emit_struct_field("cache.vtable", "&__metal_driver_vtable_sifive_fu540_c000_l2");
+	  emit_struct_field("cache.vtable", "&__metal_driver_vtable_sifive_fu540_c000_l2.cache");
 
 	  n.named_tuples(
 	    "reg-names", "reg",
