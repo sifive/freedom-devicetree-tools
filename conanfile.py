d2cd98c8072653c7bfb044f3da8248d52e0fe544
Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>
2019-10-02 17:50:17 +0200

    update conan package recipe
    Signed-off-by: Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>

--- conanfile.py
+++ conanfile.py
@@ -1,32 +1,44 @@
 import os
 from conans import ConanFile
-from conans.errors import ConanInvalidConfiguration
+from conans.errors import ConanInvalidConfiguration, ConanException
 from conans.tools import os_info, SystemPackageTool
 
 class FreedomDevicetreeToolsConan(ConanFile):
     name = "freedom-devicetree-tools"
-    # ~ version = "8.2.0"
-    settings = "os_build", "arch_build"
+    version = "v201905"
+    settings = "os"
     url = None # We might consider pointing on toolchain repo here
     description = """Freedom devicetree tools package"""
+    # exports_sources is use to build from source
+    exports_sources = "*"
     license = "MIT"
     
     def build(self):
-        self.run("./configure")
-        self.run("make")
+        if(self.settings.os != "Linux"):
+            raise ConanInvalidConfiguration("unsupported os: %s" %
+                                            (self.settings.os))
+        try : 
+            self.run("./configure")
+        except ConanException:
+            self.run("make distclean")
+            self.run("./configure")
+        self.run("make -j")
 
-    # ~ def package_id(self):
-        # ~ del self.info.settings.build_type
+    def package(self):
+        # Copy metal includes 
+        self.copy("freedom-*", dst="bin", src="", excludes= "*.c++")
 
-    # ~ def package(self):
-        # ~ # Copy metal includes 
-        # ~ self.copy("*.h", dst="include/metal", src="../metal/*")
-        # ~ self.copy("*", dst="", src=self.name)
-
-    # ~ def package_info(self):
-                                        
+    def package_info(self):
+        if(self.settings.os != "Linux"):
+            raise ConanInvalidConfiguration("unsupported os: %s" %
+                                            (self.settings.os))
+                                            
+        # ~ self.env_info.path.append(self.package_folder)                                
+        # ~ bin_folder = os.path.join(self.package_folder, "bin")
+        # ~ self.env_info.path.append(bin_folder)
+                                            
     def requirements(self):
-        print("requirements")
+
         if (os_info.linux_distro in ("debian", "ubuntu")) :
             pack_name = ["device-tree-compiler", "libfdt-dev"]
         if( os_info.linux_distro in ("arch", "manjaro")) :
