f14c83eccca2bb63e215beb832a6f4568965973a
Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>
2019-10-04 16:17:24 +0200

    add packaging method to conanfile.py
    Signed-off-by: Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans.tools import os_info, SystemPackageTool
 class FreedomDevicetreeToolsConan(ConanFile):
     name = "freedom-devicetree-tools"
     version = "v201905"
-    settings = "os"
+    settings = "os_build"
     url = None # We might consider pointing on toolchain repo here
     description = """Freedom devicetree tools package"""
     # exports_sources is use to build from source
@@ -14,31 +14,28 @@ class FreedomDevicetreeToolsConan(ConanFile):
     license = "MIT"
     
     def build(self):
-        if(self.settings.os != "Linux"):
-            raise ConanInvalidConfiguration("unsupported os: %s" %
-                                            (self.settings.os))
+        if(self.settings.os_build != "Linux"):
+            raise ConanInvalidConfiguration("unsupported os_build: %s" %
+                                            (self.settings.os_build))
         try : 
             self.run("./configure")
         except ConanException:
             self.run("make distclean")
             self.run("./configure")
-        self.run("make -j")
+        self.run("make")
 
     def package(self):
         # Copy metal includes 
         self.copy("freedom-*", dst="bin", src="", excludes= "*.c++")
 
     def package_info(self):
-        if(self.settings.os != "Linux"):
-            raise ConanInvalidConfiguration("unsupported os: %s" %
-                                            (self.settings.os))
-                                            
-        # ~ self.env_info.path.append(self.package_folder)                                
-        # ~ bin_folder = os.path.join(self.package_folder, "bin")
-        # ~ self.env_info.path.append(bin_folder)
-                                            
+        if(self.settings.os_build != "Linux"):
+            raise ConanInvalidConfiguration("unsupported os_build: %s" %
+                                            (self.settings.os_build))
+        bin_folder = os.path.join(self.package_folder, "bin")
+        self.env_info.path.append(bin_folder)
+        
     def requirements(self):
-
         if (os_info.linux_distro in ("debian", "ubuntu")) :
             pack_name = ["device-tree-compiler", "libfdt-dev"]
         if( os_info.linux_distro in ("arch", "manjaro")) :
