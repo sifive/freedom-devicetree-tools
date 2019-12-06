885da1a40258d1717d94a77b7e8293200593be70
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-12-06 11:12:59 -0800

    Replace old tests with updated Devicetrees
    Tests now include all freedom-e-sdk targets and standard cores,
    including both RTL testbench and Arty targets.
    
    Test scripts are also more generic, allowing for common tests to be
    added to the scripts tests/*.bash and target-specific tests to be added
    to the scripts in each tests/TARGET/*.bash.
    
    At the moment, all tests merely run the tools and check that they don't
    error.
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- tests/e31-memory-openocd.bash
+++
@@ -1,9 +0,0 @@
-set -e
-
-tempdir="$(mktemp -d)"
-trap "rm -rf $tempdir" EXIT
-
-cd "$tempdir"
-
-dtc $SOURCE_DIR/tests/e31-memory.dts -o e31-memory.dtb -O dtb
-$WORK_DIR/freedom-openocdcfg-generator -b arty -d e31-memory.dtb -o e31-memory-openocd.cfg
--- tests/hifive1-makeattributes.bash
+++
@@ -1,9 +0,0 @@
-set -e
-
-tempdir="$(mktemp -d)"
-trap "rm -rf $tempdir" EXIT
-
-cd "$tempdir"
-
-dtc $SOURCE_DIR/tests/hifive1.dts -o hifive1.dtb -O dtb
-$WORK_DIR/freedom-makeattributes-generator -d hifive1.dtb -o hifive1-build.env
